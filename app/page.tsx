9f35c5ab0facf6fa2abb0304abb2f44477df15af
RaghavOG <04raghavsingla28@gmail.com>
2025-01-01 17:20:06 +0530

    feat :Added Suggested User (right sidebar)

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
-export default function Home() {
+import WhoToFollow from "@/components/WhoToFollow";
+import { getDbUserId } from "@/actions/user.action";
+import { currentUser } from "@clerk/nextjs/server";
+import CreatePost from "@/components/CreatePost";
+
+export default async function Home() {
+  const user = await currentUser();
+    const dbUserId = await getDbUserId();
+
+
   return (
-  <>
-  
-  </>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+      {user ? <CreatePost /> : null}
+
+        <div className="space-y-6">
+
+        </div>
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
+      </div>
+    </div>
   );
 }
