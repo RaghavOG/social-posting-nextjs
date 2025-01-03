c246374ae025a177f6c2ce96fb827572f97f2323
RaghavOG <04raghavsingla28@gmail.com>
2025-01-03 19:30:11 +0530

    fix: changes in middleware.tsx

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,10 @@ import { clerkMiddleware } from '@clerk/nextjs/server';
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/', '/api/(.*)', '/((?!_next/static|favicon.ico|.*\\..*).*)'],
+  matcher: [
+    '/', 
+    '/api/(.*)', 
+    '/((?!_next/static|favicon.ico|.*\\..*).*)', 
+    '/(.*)',
+  ],
 };
